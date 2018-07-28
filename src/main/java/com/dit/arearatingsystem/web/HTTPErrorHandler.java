558449e6fb0626a3cfdfb813bed4f695482cf5c9
Milan Kotarac <kotaracmilan@hotmail.com>
2018-07-28 02:55:41 +0200

    registration

--- src/main/java/com/dit/arearatingsystem/web/HTTPErrorHandler.java
+++ src/main/java/com/dit/arearatingsystem/web/HTTPErrorHandler.java
@@ -2,15 +2,19 @@ package com.dit.arearatingsystem.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HTTPErrorHandler {
 
 	  
-	 @RequestMapping(value="/404")
-	 public String error404(){
-	  return "/404";
+	 @RequestMapping(value="/404", method= RequestMethod.GET)
+	 public String error404get(){
+	  return "404";
 	 }
 	  
-	
+	 @RequestMapping(value="/404", method= RequestMethod.POST)
+	 public String error404post(){
+	  return "404";
+	 }
 }
